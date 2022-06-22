f9bd1a9451a6d47d01a54780950036f4ec8a0a39
Alena Elena <alenaelena13.01@gmail.com>
2022-06-22 22:10:00 +0300

    added agency-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,56 +1,45 @@
 
-// document.querySelectorAll('.gallery__item').forEach(n => {
-//   const thumbs = new Swiper(n.querySelector('.gallery-slider__small'), {
-//       slidesPerView: 3,
-//       loop: true,
-//       freeMode: true,
-//       watchSlidesProgress: true,   
-//   });
+document.querySelectorAll('.gallery__item-slider').forEach(n => {
+  const thumbs = new Swiper(n.querySelector('.gallery-slider__small'), {
+      spaceBetween: 20,
+      slidesPerView: 3,
+      loop: true,
+      freeMode: true,
+      watchSlidesProgress: true, 
 
-//   const slider = new Swiper(n.querySelector('.gallery-slider__big'), {
-//       slidesPerView: 1,
-//       loop: true,
-//       navigation: {
-//         nextEl: ".gallery-slider__button-next",
-//         prevEl: ".gallery-slider__button-prev",
-//       },
-//       thumbs: {
-//         swiper: thumbs,
-//       },
-//   });
+      navigation: {
+        nextEl: '.gallery-slider__button-next',
+        prevEl: '.gallery-slider__button-prev',
+      },  
+  });
 
-//   slider.controller.control = thumbs;
-//   thumbs.controller.control = slider;
-// });
+  const slider = new Swiper(n.querySelector('.gallery-slider__big'), {
+    spaceBetween: 20,
+      slidesPerView: 1,
+      loop: true,
+  });
 
-
-const swiper = new Swiper(".gallery-slider__small", {
-    slidesPerView: 3,
-    loop: true,
-    freeMode: true,
-    watchSlidesProgress: true,
-    navigation: {
-      nextEl: ".gallery-slider__button-next",
-      prevEl: ".gallery-slider__button-prev",
-    },
-});
-const swiper2 = new Swiper(".gallery-slider__big", {
-    slidesPerView: 1,
-    loop: true,
-
-    thumbs: {
-      swiper: swiper
-    }
+  slider.controller.control = thumbs;
+  thumbs.controller.control = slider;
 });
 
-
-
-
-
-
-
-
-
+const sliderModal = new Swiper('.slider-modal', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  freeMode: true,
+  watchSlidesProgress: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    type: 'bullets',
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 const headerMenu = document.querySelector('.header__menu');
 const burgerBtn = document.querySelector('.burger-btn');
@@ -64,26 +53,28 @@ headerMenuClose.addEventListener('click', function () {
 
 
 const overlay = document.querySelector('.overlay')
-const items = document.querySelectorAll('.popup-item')
-items.forEach(item => {
-    const popup = item.querySelector('.popup');
-    const openPopup = item.querySelector('.popup-open');
-  
-    openPopup.addEventListener('click', function(e){
-      openModal(popup)
-     e.preventDefault();
-    })
+//const items = document.querySelectorAll('.popup-item')
+
+document.querySelectorAll('.popup-item').forEach(item  =>
+  {
+      const popup = item.querySelector('.popup');
+      const openPopup = item.querySelector('.popup-open');
     
-    const closePopup = item.querySelector('.popup-close');
-    closePopup.addEventListener('click', function(e){
-      closeModal(popup)
+      openPopup.addEventListener('click', function(e){
+        openModal(popup)
       e.preventDefault();
-    })
-  
-    overlay.addEventListener('click', function(){
-      closeModal(popup)
-    })
-});
+      })
+      
+      const closePopup = item.querySelector('.popup-close');
+      closePopup.addEventListener('click', function(e){
+        closeModal(popup)
+        e.preventDefault();
+      })
+    
+      overlay.addEventListener('click', function(){
+        closeModal(popup)
+      })
+  });
 
 function openModal(popup) {
   if (popup == null) return;
