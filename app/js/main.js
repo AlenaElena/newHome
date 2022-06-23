0f889ce2362daac33250bb72afb33402c1076460
Alena Elena <alenaelena13.01@gmail.com>
2022-06-23 22:24:28 +0300

    completed media responsiveness home page, construction page, real-estate page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,16 @@
+      // HEADER BURGER-BTN-MENU
+const headerMenu = document.querySelector('.header__menu');
+const burgerBtn = document.querySelector('.burger-btn');
+const headerMenuClose = document.querySelector('.header__menu-close')
+burgerBtn.addEventListener('click', function() {
+  headerMenu.classList.add('header__menu--active');
+})
+headerMenuClose.addEventListener('click', function () {
+  headerMenu.classList.remove('header__menu--active');
+})
 
+
+      // AGENCY-SLIDER
 document.querySelectorAll('.gallery__item-slider').forEach(n => {
   const thumbs = new Swiper(n.querySelector('.gallery-slider__small'), {
       spaceBetween: 20,
@@ -23,6 +35,7 @@ document.querySelectorAll('.gallery__item-slider').forEach(n => {
   thumbs.controller.control = slider;
 });
 
+      // AGENCY-MODAL-SLIDER
 const sliderModal = new Swiper('.slider-modal', {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -39,54 +52,47 @@ const sliderModal = new Swiper('.slider-modal', {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
 });
 
-const headerMenu = document.querySelector('.header__menu');
-const burgerBtn = document.querySelector('.burger-btn');
-const headerMenuClose = document.querySelector('.header__menu-close')
-burgerBtn.addEventListener('click', function() {
-  headerMenu.classList.add('header__menu--active');
-})
-headerMenuClose.addEventListener('click', function () {
-  headerMenu.classList.remove('header__menu--active');
-})
-
-
-const overlay = document.querySelector('.overlay')
-//const items = document.querySelectorAll('.popup-item')
-
-document.querySelectorAll('.popup-item').forEach(item  =>
-  {
-      const popup = item.querySelector('.popup');
-      const openPopup = item.querySelector('.popup-open');
-    
-      openPopup.addEventListener('click', function(e){
-        openModal(popup)
-      e.preventDefault();
-      })
+        // MODAL-POPUP
+document.addEventListener("DOMContentLoaded", function() {
+  const overlay = document.querySelector('.overlay')
+  //const items = document.querySelectorAll('.popup-item')
+  document.querySelectorAll('.popup-item').forEach(item  =>
+    {
+        const popup = item.querySelector('.popup');
+        const openPopup = item.querySelector('.popup-open');
       
-      const closePopup = item.querySelector('.popup-close');
-      closePopup.addEventListener('click', function(e){
-        closeModal(popup)
+        openPopup.addEventListener('click', function(e){
+          openModal(popup)
         e.preventDefault();
-      })
-    
-      overlay.addEventListener('click', function(){
-        closeModal(popup)
-      })
-  });
+        })
+        
+        const closePopup = item.querySelector('.popup-close');
+        closePopup.addEventListener('click', function(e){
+          closeModal(popup)
+          e.preventDefault();
+        })
+      
+        overlay.addEventListener('click', function(){
+          closeModal(popup)
+        })
+    });
 
-function openModal(popup) {
-  if (popup == null) return;
-  popup.classList.add('popup-active')
-  overlay.classList.add('overlay-active')
-}
+  function openModal(popup) {
+    // if (popup == null) return;
+    popup.classList.add('popup-active')
+    overlay.classList.add('overlay-active')
+  }
 
-function closeModal(popup) {
-  if (popup == null) return;
-  popup.classList.remove('popup-active')
-  overlay.classList.remove('overlay-active')
-}
+  function closeModal(popup) {
+    // if (popup == null) return;
+    popup.classList.remove('popup-active')
+    overlay.classList.remove('overlay-active')
+  }
+})
+
 
 
 
